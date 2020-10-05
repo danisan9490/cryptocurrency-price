1912555512b3879fa873fd8294e85f69b246209d
Daniel <danisan9490@gmail.com>
2020-10-05 12:31:13 +0100

    #1 Create React App and Form

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import From from './components/Form/Form'
+import img from './crypto.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img
+          src={img}
+          alt="Crypto"
+        />
+      </div>
+      <div>
+        <header>Know cryptocurrencies price instantly</header>
+        <From />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Form.css'
+
+
+const Formulario = () => {
+  return (
+    <form>
+      <button
+        type="submit"
+        value="Calcular"
+      >Calculate</button>
+    </form>
+  );
+}
+
+export default Formulario; 
