be70151f518fde37506452ea01496c2b09623070
Daniel <danisan9490@gmail.com>
2020-10-05 15:35:07 +0100

    Updates

---
+++ precache-manifest.cc895da77a79fc53170b119449afa057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f71c87d58c9fe57489eea3978b98c8a",
+    "url": "/cryptocurrency-price/index.html"
+  },
+  {
+    "revision": "22b7f2560ee1eaa50187",
+    "url": "/cryptocurrency-price/static/css/main.e3187937.chunk.css"
+  },
+  {
+    "revision": "99dd3b794d36e9b34e42",
+    "url": "/cryptocurrency-price/static/js/2.d10d2c61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cryptocurrency-price/static/js/2.d10d2c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b7f2560ee1eaa50187",
+    "url": "/cryptocurrency-price/static/js/main.e5582a24.chunk.js"
+  },
+  {
+    "revision": "27997bf46c680b3b2c46",
+    "url": "/cryptocurrency-price/static/js/runtime-main.8176d59d.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/cryptocurrency-price/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
